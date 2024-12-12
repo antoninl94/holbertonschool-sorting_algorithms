72ad9370e5d68a526c9d65a501f3bbbc49301492
antoninl94 <9803@holbertonstudents.com>
2024-12-12 15:09:26 +0100

    cration of bubble_sort and 0-O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: the array to sort
+ * @size: the size of the array to sort
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, tmp, swapflag;
+
+	if (array == NULL || size < 2)
+		return;
+	for (j = 0; j < size - 1; j++)
+	{
+		swapflag = 0;
+		for (i = 0; i < size - j - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swapflag = 1;
+			}
+		}
+		if (swapflag == 0)
+			break;
+	}
+}
