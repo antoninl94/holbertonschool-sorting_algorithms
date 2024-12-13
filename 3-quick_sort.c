db0ef037470dd2c6832c4879799f0b53a5e5ad66
antoninl94 <9803@holbertonstudents.com>
2024-12-13 19:52:07 +0100

    adding quick sort prototype

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,6 +12,13 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * partition - function to split array
+ * @array: the array to split
+ * @low: index to the low of the array
+ * @high: index to the high part of the array
+ * Return: i + 1
+ */
 int partition(int *array, size_t low, size_t high)
 {
 	int pivot = array[high];
@@ -26,5 +33,12 @@ int partition(int *array, size_t low, size_t high)
 		}
 	}
 	swap(&array[i + 1], &array[high]);
-	return i + 1;
+	return (i + 1);
 }
+
+/**
+ * quick_sort - sort an array with quick sort algorithm
+ * @array: the array to sort
+ * @size: size of the array;
+ */
+void quick_sort(int *array, size_t size)
