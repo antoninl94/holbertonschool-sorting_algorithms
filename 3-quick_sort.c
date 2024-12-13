8a6ee4b6d7252f1bb0624d2d763a0b9bc1bfd3f5
antoninl94 <9803@holbertonstudents.com>
2024-12-13 15:02:15 +0100

    creation of quick_sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+/**
+ * swap - swapt two elements
+ * @a: first element to swap
+ * @b: seconde element to swap
+ */
+void swap(size_t *a, size_t *b)
+{
+	size_t t = *a;
+	size_t *a = *b;
+	*b = t;
+}
+
+
+
+/**
+ * 
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
