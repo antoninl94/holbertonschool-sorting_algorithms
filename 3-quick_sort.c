1eb0087bf2244fd83170828fcb3b985f3c40a32b
antoninl94 <9803@holbertonstudents.com>
2024-12-13 15:16:16 +0100

    adding return

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,9 +15,13 @@ void swap(size_t *a, size_t *b)
 
 
 /**
- * 
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ * @array: array to sort
+ * @size: size of the array
  */
 void quick_sort(int *array, size_t size)
 {
-
+	if (array == NULL || size < 2)
+		return;
 }
